9095c9c35cfa226ec3398c09bb8776d28c828a49
Rahul Verma <rahulverma69124@gmail.com>
2026-01-04 14:51:45 +0530

    Use environment variable for port and disable debug
    Modify app to use environment variable for port and disable debug mode.

--- app.py
+++ app.py
@@ -113,4 +113,7 @@ def predict():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True, port=7860)
+    import os
+    port = int(os.environ.get("PORT", 7860))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
